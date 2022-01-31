54ae78dc973a562313176fa750c6dc48cf31727b
Defisto7 <criska7@gmail.com>
2022-01-31 14:13:46 +0200

    master
    optimized like btn

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -19,13 +19,15 @@ export default class App extends Component {
     super(props);
     this.state = {
       data : [
-        {label: 'Going to lealrn React', important: true, id: '1'},
-        {label: 'That is so good', important: false, id: '2'},
-        {label: 'I need a break...', important: false, id: '3'}
+        {label: 'Going to lealrn React', important: true, like: false, id: '1'},
+        {label: 'That is so good', important: false, like: false, id: '2'},
+        {label: 'I need a break...', important: false, like: false, id: '3'}
       ]
     };
     this.deleteItem = this.deleteItem.bind(this);
     this.addItem = this.addItem.bind(this);
+    this.onToggleImportant = this.onToggleImportant.bind(this);
+    this.onToggleLiked = this.onToggleLiked.bind(this);
 
     this.maxId = 4;
   }
@@ -59,6 +61,25 @@ export default class App extends Component {
     })
   }
 
+  onToggleImportant(id) {
+    console.log(`Important ${id}`);
+  }
+
+  onToggleLiked(id) {
+    this.setState(({data}) => {
+      const index = data.findIndex(elem => elem.id === id);
+
+      const old = data[index];
+      const newItem = {...old, like: !old.like}
+
+      const newArr = [...data.slice(0, index), newItem, ...data.slice(index + 1)];
+
+      return {
+        data: newArr
+      }
+    })
+  }
+
   render() {
     return (
       <AppBlock>
@@ -69,7 +90,9 @@ export default class App extends Component {
         </div>
         <PostList 
           posts={this.state.data} 
-          onDelete={this.deleteItem}/>
+          onDelete={this.deleteItem}
+          onToggleImportant={this.onToggleImportant}
+          onToggleLiked={this.onToggleLiked}/>
         <PostAddForm
         onAdd={this.addItem}/>
       </AppBlock>
